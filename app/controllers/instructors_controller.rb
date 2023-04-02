dc64c8e0148c9682330e3d5a89f6a204b6d60ef1
Eva Reitzel <evareitzel@gmail.com>
2023-04-02 19:24:01 -0400

    Updated code

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,62 +1,56 @@
 class InstructorsController < ApplicationController
-  
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
-  # GET /instructors
-  def index
-    instructors = Instructor.all
-    render json: instructors
-  end
-
-  # GET /instructors/:id
-  def show
-    instructor = find_instructor
-    render json: instructor
-    # rescue_from :render_not_found_response
-  end
-
-  # POST /instructors
-  def create
-    instructor = Instructor.create(instructor_params)
-    if instructor.valid?
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  
+    # GET /instructors
+    def index
+      instructors = Instructor.all
+      render json: instructors
+    end
+  
+    # GET /instructors/:id
+    def show
+      instructor = find_instructor
+      render json: instructor
+    end
+  
+    # POST /instructors
+    def create
+      instructor = Instructor.create!(instructor_params)
       render json: instructor, status: :created
-    else
-      render json: { error: instructor.errors.full_messages }, status: :unprocessable_entity
     end
-  end
-
-  # PATCH /instructors/:id
-  def update
-    instructor = find_instructor
-    instructor.update(instructor_params)
-    if instructor.valid?
-      render json: instructor, status: :accepted
-    else
-      render json: { error: instructor.errors.full_messages }, status: :unprocessable_entity
+  
+    # PATCH /instructors/:id
+    def update
+      instructor = find_instructor
+      instructor.update!(instructor_params)
+      render json: instructor
     end
+  
+    # DELETE /instructors/:id
+    def destroy
+      instructor = find_instructor
+      instructor.destroy
+      head :no_content
+    end
+    
+    private
+  
+    def find_instructor
+      Instructor.find(params[:id])
+    end
+  
+    def instructor_params
+      params.permit(:name)
+    end
+  
+    def render_not_found_response
+      render json: { errors: ["Instructor not found"] }, status: :not_found
+    end
+  
+    def render_unprocessable_entity_response(invalid)
+      render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+  
   end
-
-  # DELETE /instructors/:id
-  def destroy
-    instructor = find_instructor
-    instructor.destroy
-    render json: {}
-    # rescue_from :render_not_found_response
-  end
-
-
-  private
-
-  def instructor_params
-    params.permit(:name)
-  end
-
-  def find_instructor
-    Instructor.find(params[:id])
-  end
-
-  def render_not_found_response
-    render json: { error: "Instructor not found" }, status: :not_found
-  end
-
-end
+  
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,62 +1,56 @@
 class StudentsController < ApplicationController
-  
+
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
- 
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   # GET /students
   def index
     students = Student.all
-    render json: students, status: :accepted
+    render json: students
   end
-  
+
   # GET /students/:id
   def show
     student = find_student
-      render json: student, status: :accepted
-    # rescue_from :render_not_found_response
+    render json: student
   end
 
-# POST /students
+  # POST /students
   def create
-    student = Student.create(student_params)
-    if student.valid?
-      render json: student, status: :created
-    else
-      render json: { error: student.errors.full_messages }, status: :unprocessable_entity
-    end
+    student = Student.create!(student_params)
+    render json: student, status: :created
   end
 
   # PATCH /students/:id
   def update
     student = find_student
-    student.update(student_params)
-    if student.valid?
-      render json: student
-    else
-      render json: { error: student.errors.full_messages }, status: :unprocessable_entity
-    end
+    student.update!(student_params)
+    render json: student
   end
 
   # DELETE /students/:id
   def destroy
     student = find_student
     student.destroy
-    render json: {}
-    # rescue_from :render_not_found_response
+    head :no_content
   end
-
-
+  
   private
-
-  def student_params
-    params.permit(:name, :major, :age, :instructor_id)
-  end
 
   def find_student
     Student.find(params[:id])
   end
 
+  def student_params
+    params.permit(:name, :major, :age, :instructor_id)
+  end
+
   def render_not_found_response
-    render json: { error: "Student not found" }, status: :not_found
+    render json: { errors: ["Student not found"] }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
